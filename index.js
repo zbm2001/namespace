ec6b808bda282b34c2c942f56895bf091118d3b2
macbook <zbm2001@aliyun.com>
2019-06-25 17:04:50 +0800

    first edition.

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const global = new Function('return this')()
+
+/**
+ * namespace set or get value
+ * @param {string|Object} root
+ * @param {string|Object} sNamespace
+ * @return {Boolean} true
+ */
+module.exports = function namespace (root, sNamespace, variable) {
+  // 变量判断转化
+  if (typeof root === 'string') {
+    variable = sNamespace
+    sNamespace = root
+    root = global
+  }
+
+  if (!root || typeof sNamespace !== 'string') {
+    return
+  }
+
+  let namespaces = sNamespace.split('.')
+  let i = -1
+  let l = namespaces.length - 1
+
+  // 若未定义，则为获取命名空间
+  if (typeof variable === 'undefined') {
+    while (++i < l) {
+      root = root[namespaces[i]]
+      if (!root) {
+        return
+      }
+    }
+    return root[namespaces[l]]
+  }
+
+  while (++i < l) {
+    root = root[namespaces[i]] || (root[namespaces[i]] = {})
+  }
+  return (root[namespaces[l]] = variable)
+}
+
+module.exports.global = global
